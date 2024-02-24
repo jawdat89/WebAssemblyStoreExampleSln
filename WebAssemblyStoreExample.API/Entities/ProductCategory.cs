fa6da3d1d0d22328287c2ee179a48fb3dcf59f74
Jawdat <jawdat.89@gmail.com>
2024-02-24 21:28:41 +0200

    Add project files.

---
+++ WebAssemblyStoreExample.API/Entities/ProductCategory.cs
@@ -0,0 +1,8 @@
+﻿namespace WebAssemblyStoreExample.API.Entities
+{
+    public class ProductCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ WebAssemblyStoreExample.API/Entities/User.cs
@@ -0,0 +1,8 @@
+﻿namespace WebAssemblyStoreExample.API.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; } = string.Empty;
+    }
+}
